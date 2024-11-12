4b8a5acf495d560b8961c361c1c47d9d1842a016
leonardo <leonardo.a@cliker.mx>
2024-11-12 15:31:11 -0600

    ForwardRef y variantes

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -3,26 +3,30 @@ import { Link, router } from 'expo-router'
 import { View, Text, SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView>
-      <View className='px-10 mt-5'>
-        <Link href="/products" asChild>
-          <CustomButton color='primary'>
-            Productos
-          </CustomButton>
-        </Link>
+    return (
+        <SafeAreaView>
+            <View className='px-10 mt-5'>
+                <Link href="/products" asChild>
+                    <CustomButton className='mb-10' color='primary'>
+                      Productos
+                    </CustomButton>
+                </Link>
 
-        <CustomButton color='primary' onPress={() => router.push('/products')} >
-          Productos
-        </CustomButton>
+                <CustomButton className='mb-10' color='primary' onPress={() => router.push('/products')} >
+                  Productos
+                </CustomButton>
 
-        {/* <Link className='mb-5' href="/products">Productos</Link>
-        <Link className='mb-5' href="/profile">Perfil</Link>
-        <Link className='mb-5' href="/settings">Settings</Link> */}
-      </View>
-    </SafeAreaView>
-    
-  )
+                <CustomButton className='mb-10' variant='text-only' onPress={() => router.push('/products')} >
+                  Productos
+                </CustomButton>
+
+                {/* <Link className='mb-5' href="/products">Productos</Link>
+                <Link className='mb-5' href="/profile">Perfil</Link>
+                <Link className='mb-5' href="/settings">Settings</Link> */}
+              </View>
+        </SafeAreaView>
+      
+    )
 }
 
 export default HomeScreen
