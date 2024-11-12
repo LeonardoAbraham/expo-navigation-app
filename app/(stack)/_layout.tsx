5fa5ec646d71ea14a80bbfddb6c2f237b6440c35
leonardo <leonardo.a@cliker.mx>
2024-11-12 16:14:20 -0600

    Personalizacion del StackNavigation

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -4,7 +4,15 @@ import { Stack } from 'expo-router'
 
 const StackLayout = () => {
     return (
-        <Stack> 
+        <Stack
+            screenOptions={{
+                //headerShown: false,
+                headerShadowVisible: false,
+                contentStyle: {
+                    backgroundColor: 'white'
+                }
+            }}
+        > 
             <Stack.Screen 
                 name='home/index'
                 options={{
