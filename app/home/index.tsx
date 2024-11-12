8d62135a0562abcc777ba2fef070c60d55dcd63e
leonardo <leonardo.a@cliker.mx>
2024-11-12 14:42:12 -0600

    CustomButtons y Link AsChild

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,27 @@
-import { Link } from 'expo-router'
-import { View, Text } from 'react-native'
+import CustomButton from '@/components/shared/CustomButton'
+import { Link, router } from 'expo-router'
+import { View, Text, SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
   return (
-    <View className='px-10'>
-      <Text>HomeScreen</Text>
+    <SafeAreaView>
+      <View className='px-10 mt-5'>
+        <Link href="/products" asChild>
+          <CustomButton color='primary'>
+            Productos
+          </CustomButton>
+        </Link>
 
-      <Link className='mb-5' href="/products">Productos</Link>
-      <Link className='mb-5' href="/profile">Perfil</Link>
-      <Link className='mb-5' href="/settings">Settings</Link>
-    </View>
+        <CustomButton color='primary' onPress={() => router.push('/products')} >
+          Productos
+        </CustomButton>
+
+        {/* <Link className='mb-5' href="/products">Productos</Link>
+        <Link className='mb-5' href="/profile">Perfil</Link>
+        <Link className='mb-5' href="/settings">Settings</Link> */}
+      </View>
+    </SafeAreaView>
+    
   )
 }
 
